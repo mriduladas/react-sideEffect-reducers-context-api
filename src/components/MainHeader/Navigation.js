20ed3d89a51223feabd00aa1d6db1dcd34f76d37
mriduladas <mridula199@gmail.com>
2022-04-18 13:46:48 +0530

    Added logoutHandler to context

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -2,8 +2,8 @@ import React, {useContext} from 'react';
 import AuthContext from '../../context/auth-context';
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
-const ctx = useContext(AuthContext)
+const Navigation = () => {
+const ctx = useContext(AuthContext);
 
   return (
     <nav className={classes.nav}>
@@ -20,7 +20,7 @@ const ctx = useContext(AuthContext)
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
